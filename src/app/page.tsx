ad12d6391f7ef62d0d850e2d717358336b00ae24
adi <adisudana@gmail.com>
2025-10-20 18:44:58 +0700

    add icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from './partials/hero';
 import { Services } from './partials/services';
+import { Skill } from './partials/skill';
 
 export default function Home() {
   return (
     <div className='mx-auto w-full max-w-370'>
       <Hero />
       <Services />
+      <Skill />
     </div>
   );
 }
---
+++ src/app/partials/skill.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import React from 'react';
+
+export const Skill = () => {
+  return (
+    <div className='inner-con flex'>
+      <div>
+        <div className='text-primary-200 text-lg'>Skill</div>
+        <div className='text-display-2xl font-extrabold'>
+          SKILLS THAT BRING IDEAS TO LIFE
+        </div>
+        <div className='grid grid-cols-4'>
+          <Image src='/icons/js.svg' alt='js' width={52} height={52} />
+          <Image src='/icons/css.svg' alt='js' width={52} height={52} />
+          <Image src='/icons/html.svg' alt='js' width={52} height={52} />
+          <Image src='/icons/ejs.svg' alt='js' width={52} height={52} />
+          <Image src='/icons/mongo.svg' alt='js' width={52} height={52} />
+          <Image src='/icons/react.svg' alt='js' width={52} height={52} />
+          <Image src='/icons/ts.svg' alt='js' width={52} height={52} />
+          <Image src='/icons/docker.svg' alt='js' width={52} height={52} />
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
